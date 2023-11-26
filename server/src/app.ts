a323b6e970c98c77b539fdd954cf5c6673e4291b
DNMetodiev <metodievdenis@yahoo.com>
2023-11-26 22:43:08 +0200

    Connect MongoDB to the app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,29 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import express, { Request, Response, NextFunction, Application } from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
 
 const app: Application = express();
 const PORT: number | string = process.env.PORT || 5000;
+const MONGO_URI: string = process.env.MONGO_URI || 'your_default_mongodb_uri';
 
 app.use(cors());
 app.use(express.json());
 
+
+
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello from Express Server!' });
 });
+
 
 app.use((req: Request, res: Response) => {
   res.status(404).send('Page not found');
